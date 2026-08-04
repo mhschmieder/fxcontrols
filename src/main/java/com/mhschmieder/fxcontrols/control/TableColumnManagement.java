0ab0d83b911b697ecde8e92031e7842d5821ff01
Mark Schmieder <markhschmieder@gmail.com>
2026-08-04 09:34:14 -0400

    Add some related table column management methods

---
+++ src/main/java/com/mhschmieder/fxcontrols/control/TableColumnManagement.java
@@ -0,0 +1,65 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2026 Mark Schmieder. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * This file is part of the fxcontrols Library
+ *
+ * You should have received a copy of the MIT License along with the fxcontrols
+ * Library. If not, see <https://opensource.org/licenses/MIT>.
+ *
+ * Project: https://github.com/mhschmieder/fxcontrols
+ */
+package com.mhschmieder.fxcontrols.control;
+
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.layout.StackPane;
+
+public final class TableColumnManagement {
+
+    /**
+     * The default constructor is disabled, as this is a static utilities class.
+     */
+    private TableColumnManagement() {
+    }
+
+    public static < TD > void updateColumnHeader(
+            final TableColumn< TD, ? > column,
+            final String newHeader,
+            final String newUnitsLabel ) {
+        final Node pane = column.getGraphic();
+        if ( pane instanceof final StackPane stackPane ) {
+            final Node firstChild = stackPane.getChildren().getFirst();
+            if ( firstChild instanceof final Label columnHeader ) {
+                // NOTE: This is a safe "check and set" for changing the column
+                //  header to show the name of the new unit.
+                // TODO: The TableUtilities.setTableColumnHeaderProperties
+                //  method for constructing the column potentially needs to bind
+                //  the text field to the label created to streamline setting
+                //  the header from the column.setText method.
+                columnHeader.setText(
+                        newHeader + "(" + newUnitsLabel + " )");
+            }
+        }
+    }
+}
